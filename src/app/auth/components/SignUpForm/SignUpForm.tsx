955c5d3e2b3dda7047bc70be51ba9e1ff0c79a25
Faraz <farazpourebrahimh@gmail.com>
2025-03-09 15:04:19 +0330

    [ADD] sign up page

---
+++ src/app/auth/components/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { ReactElement, FormEvent } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import signUpImage from "@/assets/images/sign-up.webp";
+
+import { Button } from "@/components/Button/Button";
+import Card from "@/components/Card/Card";
+import NormalInputComponent from "@/components/NormalInput/NormalInput";
+import PasswordInputComponent from "@/components/PasswordInput/PasswordInput";
+
+import MingcuteIncognitoModeLine from "@/icons/MingcuteIncognitoModeLine";
+import MingcuteUser3Line from "@/icons/MingcuteUser3Line";
+import MingcuteMailLine from "@/icons/MingcuteMailLine";
+
+import styles from "@/app/auth/styles/auth-form.module.css";
+
+export default function SignUpForm(): ReactElement {
+  const formSubmitHandler = async (
+    e: FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles["auth-form"]}>
+      <Card>
+        <div className={styles["card-content"]}>
+          <div className={styles.writings}>
+            <h1>ثبت‌نام!</h1>
+            <form onSubmit={formSubmitHandler}>
+              <NormalInputComponent
+                label="نام و نام خانوادگی"
+                type="text"
+                name="name"
+                prefixIcon={<MingcuteIncognitoModeLine />}
+              />
+              <NormalInputComponent
+                label="نام کاربری"
+                type="text"
+                name="username"
+                prefixIcon={<MingcuteUser3Line />}
+              />
+              <NormalInputComponent
+                label="ایمیل"
+                type="email"
+                name="email"
+                prefixIcon={<MingcuteMailLine />}
+              />
+              <PasswordInputComponent
+                label="رمز عبور"
+                name="password"
+                autoComplete="new-password"
+              />
+              <Button variant="primary">ثبت‌نام</Button>
+            </form>
+            <div className={styles["change-form"]}>
+              قبلاً ثبت‌نام کردید؟
+              {` `}
+              <Link href="/auth/sign-in">وارد شوید</Link>.
+            </div>
+          </div>
+          <div className={styles.visuals}>
+            <Image src={signUpImage} alt="" />
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
