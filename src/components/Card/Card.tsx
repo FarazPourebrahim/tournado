cb70ce8b183d4e34642ea1961e637db43c8c7cf2
Faraz <farazpourebrahimh@gmail.com>
2025-03-09 15:03:48 +0330

    [ADD] card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,35 +1,23 @@
-import styles from "./Card.module.css";
-import Image from "next/image";
-import Link from "next/link";
-import { MockTour } from "@/mocks/mockTours";
+import { PropsWithChildren, ReactElement } from "react";
 
-export default function Card({ tour }: { tour: MockTour }) {
-  const { id, title, location, price, duration, guideAvailable, type, image } =
-    tour;
+import clsx from "clsx";
 
+import styles from "./card.module.css";
+
+type Props = PropsWithChildren<{
+  className?: string;
+  title?: string;
+}>;
+
+export default function CardComponent({
+  children,
+  className,
+  title,
+}: Props): ReactElement {
   return (
-    <Link href={`/tour/${id}`} className={styles.card}>
-      <Image
-        src={image}
-        alt={title}
-        className={styles.image}
-        width={400}
-        height={400}
-      />
-      <div className={styles.content}>
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.details}>
-          <span>{location}</span>
-          <span>/</span>
-          <span>{duration} روز</span>
-          <span>/</span>
-          <span>{guideAvailable ? "با راهنما" : "بدون راهنما"}</span>
-        </p>
-        <div className={styles.bottom}>
-          <p className={styles.price}>{price.toLocaleString()} تومان</p>
-          <p className={styles.type}>{type.label}</p>
-        </div>
-      </div>
-    </Link>
+    <div className={styles.wrapper}>
+      <div className={styles.title}>{title}</div>
+      <div className={clsx(styles.card, className)}>{children}</div>
+    </div>
   );
 }
