1f175990fb132d381e92abaafbe4408b6b09665b
Faraz <farazpourebrahimh@gmail.com>
2025-03-19 18:00:49 +0330

    [ADD] toast

--- src/app/auth/components/SignUpForm/SignUpForm.tsx
+++ src/app/auth/components/SignUpForm/SignUpForm.tsx
@@ -5,12 +5,16 @@ import { ReactElement, FormEvent } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { toast } from "react-toastify";
+
 import signUpImage from "@/assets/images/sign-up.webp";
 
 import { Button } from "@/components/Button/Button";
 import Card from "@/components/Card/Card";
 import NormalInputComponent from "@/components/NormalInput/NormalInput";
 import PasswordInputComponent from "@/components/PasswordInput/PasswordInput";
+
+import { SignUpDto } from "@/dto/auth";
 
 import MingcuteIncognitoModeLine from "@/icons/MingcuteIncognitoModeLine";
 import MingcuteUser3Line from "@/icons/MingcuteUser3Line";
@@ -23,6 +27,35 @@ export default function SignUpForm(): ReactElement {
     e: FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const dto: SignUpDto = {
+      name: formData.get("name") as string,
+      username: formData.get("username") as string,
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+    };
+
+    const response = await fetch("/api/auth/sign-up", {
+      method: "POST",
+      body: JSON.stringify(dto),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      let message: string = "خطای غیرمنتظره رخ داد.";
+
+      if ("error" in result) {
+        message = result.error;
+      }
+
+      toast.error(message);
+      return;
+    }
+
+    toast.success("ثبت‌نام با موفقیت انجام شد.");
   };
 
   return (
