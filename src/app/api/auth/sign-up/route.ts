f5dc151dc5e8e7edc470a61ae2d8ba1850a307ab
Faraz <farazpourebrahimh@gmail.com>
2025-03-19 17:55:04 +0330

    [ADD] sign up

---
+++ src/app/api/auth/sign-up/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { SignUpDto } from "@/dto/auth";
+import prisma from "@/lib/prisma";
+import { ApiResponseType } from "@/types/api-response";
+import { parseBody, wrapWithTryCatch } from "@/utils/api";
+
+export async function POST(request: Request): Promise<ApiResponseType<null>> {
+  return wrapWithTryCatch(async () => {
+    const [parseError, body] = await parseBody<SignUpDto>(request);
+
+    if (parseError !== null) {
+      return NextResponse.json({ error: parseError }, { status: 400 });
+    }
+
+    let foundUser = await prisma.user.findUnique({
+      where: { username: body.username },
+    });
+
+    if (foundUser) {
+      return NextResponse.json(
+        { error: "نام کاربری تکراری است." },
+        { status: 400 },
+      );
+    }
+
+    foundUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    if (foundUser) {
+      return NextResponse.json({ error: "ایمیل تکراری است." }, { status: 400 });
+    }
+
+    await prisma.user.create({ data: body });
+
+    return NextResponse.json({ data: null }, { status: 201 });
+  });
+}
